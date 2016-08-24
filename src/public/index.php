c67afbc56ea72f307662751bb0b36f88f5da9185
Andreas von Burg <a@von-burg.net>
2016-08-24 12:41:40 +0200

    N2N::setup() removed

--- src/public/index.php
+++ src/public/index.php
@@ -11,15 +11,14 @@ if (isset($_SERVER['N2N_STAGE'])) {
 	define('N2N_STAGE', $_SERVER['N2N_STAGE']);
 }
 
-require_once 'n2n/N2N.php';
+require_once 'n2n/core/TypeLoader.php';
 
-n2n\N2N::setup($pubPath, $varPath);
-n2n\N2N::initialize(new n2n\core\FileN2nCache());
+n2n\core\TypeLoader::register();
 
-n2n\N2N::autoInvokeBatchJobs();
-n2n\N2N::autoInvokeControllers(); 
-
-n2n\N2N::finalize();
+n2n\core\N2N::initialize($pubPath, $varPath, new n2n\core\FileN2nCache());
+n2n\core\N2N::autoInvokeBatchJobs();
+n2n\core\N2N::autoInvokeControllers();
+n2n\core\N2N::finalize();
 
 function test($value) {
 	if (n2n\N2N::isLiveStageOn()) return;
