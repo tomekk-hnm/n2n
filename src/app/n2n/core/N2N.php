7ee3637fd4c35a6615f219db24f1c9eca1e10ed1
Andreas von Burg <a@von-burg.net>
2018-09-14 14:50:47 +0200

    version updated

--- src/app/n2n/core/N2N.php
+++ src/app/n2n/core/N2N.php
@@ -57,7 +57,7 @@ use n2n\core\module\impl\EtcModuleFactory;
 define('N2N_CRLF', "\r\n");
 
 class N2N {
-	const VERSION = '7.2.0-beta.3';
+	const VERSION = '7.2.0-beta.4';
 	const LOG4PHP_CONFIG_FILE = 'log4php.xml'; 
 	const LOG_EXCEPTION_DETAIL_DIR = 'exceptions';
 	const LOG_MAIL_BUFFER_DIR = 'log-mail-buffer';
